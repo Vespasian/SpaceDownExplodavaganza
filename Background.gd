75aa8911b18f17085833695832364f69a6d63b4a
Vespasian <Vespasian@users.noreply.github.com>
2019-11-22 21:11:42 +0100

    work on background

---
+++ Background.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+var scrollSpeed = 10
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var tiles = []
+
+var screenWidth = 0
+var screenHeight = 0
+var tileHeight = 0
+
+var moveTiles = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	screenWidth = get_viewport().size.x
+	screenHeight = get_viewport().size.y
+
+	position.x = 0
+	position.y = 0
+	# fill the screen
+	var tile = get_child(0)
+	tileHeight = tile.texture.get_height() * tile.scale.y
+	
+	var y = 0
+	#while (y < screenHeight):
+	#	addTileRow(y, tile)
+	#	y += tileHeight
+	addTileRow(0, tile)
+	addTileRow(tileHeight, tile)
+	addTileRow(tileHeight*2, tile)
+	addTileRow(tileHeight*3, tile)
+	addTileRow(tileHeight*4, tile)
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	moveTiles += delta * scrollSpeed
+	if ( moveTiles < 1):
+		return
+	
+	var diff = floor(moveTiles)
+	moveTiles = 0
+	
+	for tile in tiles:
+		tile.position.y += diff
+		if ( tile.position.y > screenHeight):
+			tile.position.y =  0
+	pass
+	
+func addTileRow(y, tile):
+	var screenWidth = get_viewport().size.x
+	var tileWidth = tile.texture.get_width() * tile.scale.x
+	var x = 0
+	while (x < screenWidth):
+		var newTile = tile.duplicate()
+		newTile.visible = true
+		newTile.position.y = y
+		newTile.position.x = x
+		add_child(newTile)
+		tiles.append(newTile)
+		x += tileWidth
+	pass
