3467df87507a2d9bb01715ce1253546e0ae6b68b
Robert Jacob <robert.jacob@holidaycheck.com>
2019-11-22 21:43:53 +0100

    Automatically spawn enemies

--- Fleischball.gd
+++ Fleischball.gd
@@ -1,14 +1,13 @@
-extends Sprite
+extends Node2D
 
 # Declare member variables here. Examples:
 var dx = 0
 var dy = 30
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	position.x = 200
-	position.y = 200
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += dx * delta
 	position.y += dy * delta
+	
+	if position.y > get_viewport().size.y:
+		queue_free()
--- shot.gd
+++ shot.gd
@@ -3,10 +3,6 @@ extends Node2D
 var dx = 0
 var dy = -500
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += dx * delta
