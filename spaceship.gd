7191bee86e9ac64c2450e7993066982388c9a820
Markus Klein <markus.klein@seitenbau.com>
2019-11-22 18:17:08 +0100

    Set initial position of spaceship

--- spaceship.gd
+++ spaceship.gd
@@ -5,6 +5,10 @@ var speed = 250
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var screenWidth = get_viewport().size.x
+	var screenHeight = get_viewport().size.y
+	position.x = screenWidth / 2
+	position.y = screenHeight - 75
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
