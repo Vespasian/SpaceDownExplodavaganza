65aaef868cad22511fbfd5e2501b3e1a5852b63f
Fabian Willke <fwillke@gmail.com>
2019-11-22 22:24:59 +0100

    bla

--- Fleischball.gd
+++ Fleischball.gd
@@ -3,11 +3,14 @@ extends Node2D
 # Declare member variables here. Examples:
 var dx = 0
 var dy = 30
+var spriteSize
 
+func _ready():
+	spriteSize = get_node("Sprite").texture.get_size().y
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += dx * delta
 	position.y += dy * delta
 	
-	if position.y > get_viewport().size.y:
+	if position.y > get_viewport().size.y + spriteSize:
 		queue_free()
