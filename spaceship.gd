c7b5bb59294af62bbd227849fb47c7a531d5f19b
Robert Jacob <robert.jacob@holidaycheck.com>
2019-11-22 18:04:11 +0100

    Simple movement

---
+++ spaceship.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+# Movement speed
+var speed = 250
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
