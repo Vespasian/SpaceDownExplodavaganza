93c14232fb238d2f01d87ddf2ee0a890baaefcfe
Markus Klein <markus.klein@seitenbau.com>
2019-11-22 22:11:34 +0100

    Kill those fleischbaellchen

--- Fleischball.gd
+++ Fleischball.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Declare member variables here. Examples:
 var dx = 0
 var dy = 30
+signal enemy_hit
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -11,3 +12,6 @@ func _process(delta):
 	
 	if position.y > get_viewport().size.y:
 		queue_free()
+
+func _on_Fleischball_area_entered(area):
+	queue_free()
