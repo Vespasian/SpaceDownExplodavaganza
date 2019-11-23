1b5ca7a9ca0635685833a65b7bb54f4c2c0feaf6
Robert Jacob <robert.jacob@holidaycheck.com>
2019-11-23 10:56:34 +0100

    Fix spaceship clamp

--- spaceship.gd
+++ spaceship.gd
@@ -4,24 +4,18 @@ extends Area2D
 # Signal, was ausgesendet wird, wenn man getroffen wird
 signal hit
 var speed = 500
-var screenWidth
-var screenHeight
-var sizeX
-var sizeY
+var viewportSize
+var spriteSize
 var shotScene = preload("res://shot.tscn")
 var dead = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screenHeight = get_viewport().size.y
-	screenWidth = get_viewport().size.x
-	position.x = screenWidth / 2
-	position.y = screenHeight - 75
+	viewportSize = get_viewport().size
+	position.x = viewportSize.x / 2
+	position.y = viewportSize.y - 75
 	var sprite = get_node("Sprite")
-	var spriteScale = sprite.scale
-	sizeX = sprite.texture.get_size().x * spriteScale.x
-	sizeY = sprite.texture.get_size().y * spriteScale.y
-	
+	spriteSize = sprite.get_rect().size * sprite.scale
 
 func _process(delta):
 	if dead:
@@ -34,21 +28,19 @@ func _process(delta):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
-	position.x = clamp(position.x, 0 + (sizeX / 2), screenWidth - (sizeX / 2))
-	position.y = clamp(position.y, 0 + (sizeY / 2), screenHeight - (sizeY / 2))
+	position.x = clamp(position.x, spriteSize.x / 2, viewportSize.x - (spriteSize.x / 2))
+	position.y = clamp(position.y, spriteSize.y / 2, viewportSize.y - (spriteSize.y / 2))
 	
 	if Input.is_action_just_pressed("player_shoot"):
 		var shot = shotScene.instance()
 		shot.position.x = position.x
-		shot.position.y = position.y - sizeY + 19
+		shot.position.y = position.y - spriteSize.y + 19
 		get_parent().add_child(shot)
-
 
 func _on_spaceship_area_entered(area):
 	if not area.get_name() == "shot":
 		emit_signal("hit")
 		print("hit")
-
 
 func _on_health_dead():
 	dead = true
