ff2a0b837073e3f5c64421684963f10c42070436
Fabian Willke <fwillke@gmail.com>
2019-11-23 08:49:08 +0100

    health added

---
+++ health.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	health = 5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
