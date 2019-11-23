5f8f328c725bebc263500e6a6293a04d5cb49ff0
Fabian Willke <fwillke@gmail.com>
2019-11-23 13:35:59 +0100

    add music

--- music.gd
+++ music.gd
@@ -1,4 +1,4 @@
-extends AudioStreamPlayer2D
+extends AudioStreamPlayer
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,7 +6,7 @@ extends AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
